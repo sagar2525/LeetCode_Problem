e67cacbd6791aaeb5a434cca431fe76166b96a34
Sagar singh Kushwaha <83105756+sagar2525@users.noreply.github.com>
2025-08-01 00:54:51 +0530

    Time: 0 ms (100%), Space: 13.9 MB (82.93%) - LeetHub

---
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+void solve(vector<int>& candidates, int target,vector<vector<int>>& ans,vector<int> &v,int index){
+    // base case
+    if(target==0){
+        ans.push_back(v);
+        return;
+    }
+    if(target<0){
+        return;
+    }
+    for(int i=index;i<candidates.size();i++){
+        if(i > index && candidates[i] == candidates[i-1]){
+            continue;
+        }
+        v.push_back(candidates[i]);
+        solve(candidates,target-candidates[i],ans,v,i+1);
+        v.pop_back();
+    }
+    
+}
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        sort(candidates.begin(),candidates.end());
+        vector<vector<int>> ans;
+        vector<int> v;
+        solve(candidates,target,ans,v,0);
+        // set<vector<int>>st;
+        // for(auto e:ans){
+        //     st.insert(e);
+        // }
+
+        // ans.clear();
+
+        // for(auto e:st){
+        //     ans.push_back(e);
+        // }
+        return ans;
+    }
+};
