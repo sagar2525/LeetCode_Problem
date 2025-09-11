364cdfc0221c9e5e748d93fcf63b1b415e77dec8
Sagar singh Kushwaha <83105756+sagar2525@users.noreply.github.com>
2025-09-12 00:07:15 +0530

    Time: 3 ms (61.65%), Space: 23.2 MB (76.96%) - LeetHub

--- 0155-min-stack/0155-min-stack.cpp
+++ 0155-min-stack/0155-min-stack.cpp
@@ -1,18 +1,17 @@
 class MinStack {
 public:
-    vector<pair<int,int>> st;
-
+vector<pair<int,int>> st;
     MinStack() {
         
     }
     
     void push(int val) {
         if(st.empty()){
-            pair<int , int> p = make_pair(val , val);
+            pair<int ,int > p = make_pair(val , val);
             st.push_back(p);
         }
         else{
-            pair<int , int> p;
+            pair<int ,int>p;
             p.first = val;
             p.second = min(val , st.back().second);
             st.push_back(p);
