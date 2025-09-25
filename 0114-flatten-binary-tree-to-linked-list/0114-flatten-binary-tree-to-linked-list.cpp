f7c05d75fe9f911a8b32706a585179c2fc0495f3
Sagar singh Kushwaha <83105756+sagar2525@users.noreply.github.com>
2025-09-25 22:47:33 +0530

    Time: 0 ms (100%), Space: 17.9 MB (9.29%) - LeetHub

---
+++ 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    void solvetree(TreeNode* root, vector<TreeNode*>& nodes) {
+        if(!root) return;
+        nodes.push_back(root);
+        solvetree(root->left, nodes);
+        solvetree(root->right, nodes);
+    }
+
+    void flatten(TreeNode* root) {
+        if(root==NULL){
+            return;
+        }
+        vector<TreeNode*> nodes;
+        solvetree(root, nodes);
+
+        for(int i=0; i < nodes.size() - 1; i++) {
+            nodes[i]->left = NULL;
+            nodes[i]->right = nodes[i+1];
+        }
+        // Handle last node
+        nodes.back()->left = NULL;
+        nodes.back()->right = NULL;
+    }
+};
