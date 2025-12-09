b212fcda31eda4907cad42b4051bc45ee8408b93
Sagar singh Kushwaha <83105756+sagar2525@users.noreply.github.com>
2025-12-09 17:45:56 +0530

    Time: 113 ms (30.3%), Space: 51.4 MB (35.67%) - LeetHub

--- 0632-smallest-range-covering-elements-from-k-lists/0632-smallest-range-covering-elements-from-k-lists.cpp
+++ 0632-smallest-range-covering-elements-from-k-lists/0632-smallest-range-covering-elements-from-k-lists.cpp
@@ -1,49 +1,45 @@
 class info{
     public:
         int data;
-        int rindex;
         int cindex;
+        int rindex;
 
-
-        info(int a,int b ,int c){
-            data = a;
-            rindex = b;
-            cindex = c;
-        }
+    info(int a, int b , int c){
+        data = a;
+        rindex = b;
+        cindex = c;
+    }
 };
 
 class compare{
     public:
-        bool operator()(info* a,info* b){
-            return a->data > b->data;
-        }
+    bool operator()(info* a, info* b){
+        return a->data > b->data;
+    }
 };
-
 class Solution {
 public:
     vector<int> smallestRange(vector<vector<int>>& nums) {
         priority_queue<info* , vector<info*> , compare> pq;
         vector<int> ans;
+
         int maxi = INT_MIN;
         int mini = INT_MAX;
 
-        // proccess first k elemnt 
         int totalrow = nums.size();
-        int totalcolumn = nums[0].size();
 
-        for(int row =0 ; row<totalrow ;row++){
+        for(int row=0;row<totalrow;row++){
             int element = nums[row][0];
-            info* temp = new info(element , row , 0);
+            info* temp = new info(element , row ,0);
             pq.push(temp);
-            maxi = max(maxi,element);
-            mini = min(mini,element);
+            maxi = max(maxi , element);
+            mini = min(mini , element);
         }
 
+        // main logic 
         int ans_start = mini;
         int ans_end = maxi;
 
-
-        // proccessing
         while(!pq.empty()){
             info* front = pq.top();
             pq.pop();
@@ -53,25 +49,24 @@ public:
             int front_cindex = front->cindex;
 
             mini = frontdata;
-            // ans update 
-            if((maxi-mini)<(ans_end-ans_start)){
-                ans_start = mini;
-                ans_end = maxi;
 
+            if((maxi-mini)< (ans_end - ans_start)){
+                ans_start = mini;
+                ans_end = maxi; 
             }
 
-            int currtotalcolumns = nums[front_rindex].size();
-
-            if(front_cindex+1<currtotalcolumns){
+            int totalcolumn = nums[front_rindex].size();
+            // aage element haia to insert ker do 
+            if(front_cindex+1 < totalcolumn){
                 int element = nums[front_rindex][front_cindex+1];
-                info* temp = new info(element,front_rindex,front_cindex+1);
-                maxi = max(maxi,element);
+                info* temp = new info(element , front_rindex, front_cindex+1);
+                maxi = max(maxi , element);
                 pq.push(temp);
-
             }
             else{
                 break;
             }
+
         }
         ans.push_back(ans_start);
         ans.push_back(ans_end);
