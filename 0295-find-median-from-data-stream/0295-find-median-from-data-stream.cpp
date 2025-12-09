e74be3cd10eef63e522d09baed7298dc7c1a1907
Sagar singh Kushwaha <83105756+sagar2525@users.noreply.github.com>
2025-12-09 16:45:27 +0530

    Time: 40 ms (98.43%), Space: 148.5 MB (61.05%) - LeetHub

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.cpp
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.cpp
@@ -1,72 +1,65 @@
 class MedianFinder {
 public:
-    priority_queue<int> maxheap; // left side
-    priority_queue<int, vector<int>, greater<int>> minheap; // right side
-    double median;
 
+    priority_queue<int> maxheap;
+    priority_queue<int, vector<int> , greater<int>>minheap;
     MedianFinder() {
-        median = 0;
+        
     }
     
     void addNum(int num) {
-
-        // Case 1: both heaps same size
         if(maxheap.size() == minheap.size()){
-
-            if(num > median){
-                // insert in right
-                minheap.push(num);
-                median = minheap.top();
-            }
-            else{
-                // insert in left
-                maxheap.push(num);
-                median = maxheap.top();
-            }
-        }
-
-        // Case 2: maxheap bigger (left side has extra element)
-        else if(maxheap.size() == minheap.size() + 1){
-
-            if(num > median){
-                // insert in right (balanced automatically)
+            if(num > findMedian()){
                 minheap.push(num);
             }
             else{
-                // left already bigger → rebalance before adding
-                int element = maxheap.top();
-                maxheap.pop();
-                minheap.push(element);
-
                 maxheap.push(num);
             }
-
-            // update median (even count)
-            median = (maxheap.top() + minheap.top()) / 2.0;
         }
-
-        // Case 3: minheap bigger (right side has extra element)
-        else if(minheap.size() == maxheap.size() + 1){
-
-            if(num > median){
-                // right already bigger → rebalance before adding
+        else if(maxheap.size()+1 == minheap.size()){
+            // min heap bada hai 
+            if(num>findMedian()){
+                
                 int element = minheap.top();
                 minheap.pop();
                 maxheap.push(element);
-
                 minheap.push(num);
             }
             else{
-                // insert in left (balanced automatically)
                 maxheap.push(num);
             }
-
-            // update median (even count)
-            median = (maxheap.top() + minheap.top()) / 2.0;
+        }
+        else if(maxheap.size() == minheap.size()+1){
+            // max heap ka size bada hai 
+            if(num > findMedian()){
+                minheap.push(num);
+            }
+            else{
+                int element = maxheap.top();
+                maxheap.pop();
+                minheap.push(element);
+                maxheap.push(num);
+            }
         }
     }
     
     double findMedian() {
-        return median;
+        if(maxheap.size() == 0 && minheap.size() ==0){
+            return 0;
+        }
+        else if(maxheap.size() == minheap.size() ){
+            return (minheap.top()+maxheap.top())/2.0;
+        }
+        else if(maxheap.size()+1 == minheap.size()){
+            return minheap.top();
+        }
+        else if(maxheap.size() == minheap.size()+1){
+            return maxheap.top();
+        }
+        else{
+            return -1;
+        }
+
     }
 };
+
